9ab928b0a07e19dd4eebccd3fc41261e487e9d9f
SaraVieira <hey@iamsaravieira.com>
2025-04-25 11:56:38 +0100

    radme

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -2,31 +2,13 @@ import { it, describe } from "vitest"
 import MakerWorldJS from "../src"
 
 describe("Get models", () => {
-  it(
-    "search models",
-    async () => {
-      await new MakerWorldJS().getModels({})
-    },
-    {
-      timeout: 200000,
-    }
-  )
-  it(
-    "get user",
-    async () => {
-      await new MakerWorldJS().getUser({ user: "BambuLab" })
-    },
-    {
-      timeout: 200000,
-    }
-  )
-  it(
-    "get model",
-    async () => {
-      await new MakerWorldJS().getModel({ slug: "13717-led-lamp-001" })
-    },
-    {
-      timeout: 200000,
-    }
-  )
+  it("search models", async () => {
+    await new MakerWorldJS().getModels({})
+  }, 200000)
+  it("get user", async () => {
+    await new MakerWorldJS().getUser({ user: "BambuLab" })
+  }, 200000)
+  it("get model", async () => {
+    await new MakerWorldJS().getModel({ slug: "13717-led-lamp-001" })
+  }, 200000)
 })
